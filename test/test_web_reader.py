b2d880759c1da7372d10a5b4ebf6fdeb9ba19c3b
Michael Kukar <mkukar@gmail.com>
2020-04-26 16:47:31 -0700

    add working code and tests for addentrytodatabase

--- test/test_web_reader.py
+++ test/test_web_reader.py
@@ -36,28 +36,65 @@ class DatabaseTestCases(unittest.TestCase):
 
     EMPTY_DB_FILE = "empty_test_database.db"
 
+    VALID_DB_ENTRY = {
+        'date' : '01022020',
+        'total_cases' : 1,
+        'new_cases' : 1,
+        'new_tests' : 1,
+        'hospitalizations' : 0,
+        'intensive_care' : 5,
+        'deaths' : 0
+    }
+
+    INCOMPLETE_DB_ENTRY = {
+        'date' : '01022020',
+        'total_cases' : 1,
+        'new_cases' : 1
+    }
+
+    BAD_RANGES_DB_ENTRY = {
+        'date' : '01022020',
+        'total_cases' : -1,
+        'new_cases' : -1,
+        'new_tests' : -1,
+        'hospitalizations' : 0,
+        'intensive_care' : -5,
+        'deaths' : 0
+    }
+
     def setUp(self):
         # copies dummy database that is empty
         shutil.copyfile(self.EMPTY_DB_FILE, "temp_" + self.EMPTY_DB_FILE)
+
+        self.wr = WebReader("temp_" + self.EMPTY_DB_FILE)
 
     def tearDown(self):
         # deletes our dummy database file
         os.remove("temp_" + self.EMPTY_DB_FILE)
 
     def test_addEntryToDatabaseSucceedsWhenDatasetIsValid(self):
-        self.assertTrue(False)
+        self.assertTrue(self.wr.addEntryToDatabase(self.VALID_DB_ENTRY))
+        conn = sqlite3.connect("temp_" + self.EMPTY_DB_FILE)
+        dataset = conn.execute("select * from DATA")
+        self.assertEqual(1, len(dataset.fetchall()))
+        conn.close()
     
     def test_addEntryToDatabaseFailsWhenDatasetIsCorrupted(self):
-        self.assertTrue(False)
+        self.assertFalse(self.wr.addEntryToDatabase(None))
     
     def test_addEntryToDatabaseFailsWhenDatasetIsIncomplete(self):
-        self.assertTrue(False)
+        self.assertFalse(self.wr.addEntryToDatabase(self.INCOMPLETE_DB_ENTRY))
     
     def test_addEntryToDatabaseFailsWhenDatasetIsOutOfValidRanges(self):
-        self.assertTrue(False)
+        self.assertFalse(self.wr.addEntryToDatabase(self.BAD_RANGES_DB_ENTRY))
 
     def test_addEntryToDatabaseFailsWhenDatasetTimestampAlreadyExists(self):
-        self.assertTrue(False)
+        self.assertTrue(self.wr.addEntryToDatabase(self.VALID_DB_ENTRY))
+        self.assertFalse(self.wr.addEntryToDatabase(self.VALID_DB_ENTRY))
+        conn = sqlite3.connect("temp_" + self.EMPTY_DB_FILE)
+        dataset = conn.execute("select * from DATA")
+        self.assertEqual(1, len(dataset.fetchall()))
+        conn.close()
 
     def test_readLatestEntryFromDatabaseReturnsMostRecentTimestampFromDatabase(self):
         self.assertTrue(False)
--- web_reader.py
+++ web_reader.py
@@ -3,10 +3,51 @@
 # CB: Michael Kukar
 # Copyright Michael Kukar 2020.
 
-def WebReader():
-    
-    def addEntryToDatabase(self):
-        pass
+import sqlite3
+from collections import Mapping
+
+class WebReader:
+
+    dbFilename = "covid19.db"
+
+    REQUIRED_ENTRY_FIELDS = ['date', 'total_cases', 'new_cases', 'new_tests', 'hospitalizations', 'intensive_care', 'deaths']
+
+    ADD_ENTRY_COMMAND = ("INSERT INTO DATA (DATE, TOTAL_CASES, NEW_CASES, NEW_TESTS, HOSPITALIZATIONS, INTENSIVE_CARE, DEATHS) VALUES ("
+        ":date, :total_cases, :new_cases, :new_tests, :hospitalizations, :intensive_care, :deaths);"
+    )
+
+    def __init__(self, dbFilename):
+        # stores filename of database
+        self.dbFilename = dbFilename
+
+    def addEntryToDatabase(self, entry):
+        # makes sure entry is a dictionary
+        if entry is None or not isinstance(entry, Mapping):
+            return False
+        # makes sure entry contains all required fields (they can be None, but must exist)
+        for field in self.REQUIRED_ENTRY_FIELDS:
+            if field not in entry.keys():
+                return False
+        # checks that fields are within bounds (not negative, etc.)
+        # all fields except DATE must be greater than 0
+        for field in self.REQUIRED_ENTRY_FIELDS:
+            if field == 'date': continue
+            # makes sure entry is able to be cast to an int type
+            try:
+                int(entry[field])
+            except:
+                return False
+            if int(entry[field]) < 0:
+                return False
+        # adds entry to database
+        try:
+            conn = sqlite3.connect(self.dbFilename)
+            res = conn.execute(self.ADD_ENTRY_COMMAND, entry)
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            return False
+        return True
 
     def isNewDataAvailable(self):
         pass
